3ab8efb9964f3fc1680a78d40f38ffe6a10fef02
sahiljain112 <sahiljain112@gmail.com>
2016-10-10 23:43:10 +0530

    Updated gallery

--- gallery.php
+++ gallery.php
@@ -22,13 +22,13 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/yamm.css" />
 		<link rel="stylesheet" type="text/css" href="assets/css/camera.css"/>
 
-		
+
 
 
 
 <script type="text/javascript" src="assets/js/jquery.js"></script>
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.js"></script> 
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
 <script type="text/javascript" src="assets/js/jquery.countdown.min.js"></script>
 <script type="text/javascript" src="assets/js/wow.min.js"></script>
 <script type="text/javascript" src="assets/js/progressbars.js"></script>
@@ -37,88 +37,88 @@
 <script type="text/javascript" src="assets/js/main.js"></script>
 <script type="text/javascript" src="assets/js/camera.min.js"></script>
 <script type='text/javascript' src='assets/js/jquery.mobile.customized.min.js'></script>
-<script type='text/javascript' src='assets/js/jquery.easing.1.3.js'></script> 
+<script type='text/javascript' src='assets/js/jquery.easing.1.3.js'></script>
 
 
-	
+
 	<!--[if lt IE 9]>
 			<script src="assets/js/html5shiv.js"></script>
 			<script src="assets/js/respond.min.js"></script>
 			<![endif]-->
 	<!-- Favicon -->
 </head>
-	<body data-spy="scroll" data-target=".navbar-collapse" data-offset="100"> 
+	<body data-spy="scroll" data-target=".navbar-collapse" data-offset="100">
 	<!-- Navigation -->
- <?php 
+ <?php
   	$gallery = "active";
   	$about = "";
 	$chapters = "";
 	$branch = "";
 	$faq = "";
 	$contact = "";
-  	include('nav.php'); 
+  	include('nav.php');
   ?>
 <section class="title-section main-color">
 	<div class="container">
 		<h4>Gallery</h4>
 	</div>
-</section>	
+</section>
 
 
 
 
 
-							
+
 <section class="container wow slideInDown delay-1" >
-	
-	
-		
-		
-		
+
+
+
+
+
 		<div class="row">
 				<!-- <div class="col-md-4"></div> -->
 			<div class="col-md-12 wow shake">
 				<h2 style="font-size: 50px; font-style: bold; text-align: center;color: #6699CC">Gallery</h2>
-										
-					<div class="camera_wrap camera_azure_skin" id="camera_wrap">
 
-			            <div data-thumb="assets/img/gallery/1.jpg" data-src="assets/img/gallery/1.jpg">
-			               
+					<div class="camera_wrap camera_azure_skin" id="camera_wrap">
+								<div data-thumb="assets/img/gallery/2.jpg" data-src="assets/img/gallery/2.jpg">
+
+								</div>
+			            <div data-thumb="assets/img/gallery/IEEE_IFEST1.jpg" data-src="assets/img/gallery/IEEE_IFEST1.jpg.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/2.jpg" data-src="assets/img/gallery/2.jpg">
-			               
+			            <div data-thumb="assets/img/gallery/IEEE_IFEST2jpg" data-src="assets/img/gallery/IEEE_IFEST2.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/3.jpg" data-src="assets/img/gallery/3.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_ManualRobotics1.jpg" data-src="assets/img/gallery/IEEE_ManualRobotics1.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/4.jpg" data-src="assets/img/gallery/4.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_ManualRobotics2.jpg" data-src="assets/img/gallery/IEEE_ManualRobotics2.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/5.jpg" data-src="assets/img/gallery/5.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_Photoshop.jpg" data-src="assets/img/gallery/IEEE_Photoshop.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/6.jpg" data-src="assets/img/gallery/6.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_Sampark.jpg" data-src="assets/img/gallery/IEEE_Sampark.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/7.jpg" data-src="assets/img/gallery/7.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_SummerSchool1.jpg" data-src="assets/img/gallery/IEEE_SummerSchool1.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/8.jpg" data-src="assets/img/gallery/8.jpg">
-			                
+			            <div data-thumb="assets/img/gallery/IEEE_SummerSchool2.jpg" data-src="assets/img/gallery/IEEE_SummerSchool1.jpg">
+
 			            </div>
-			            <div data-thumb="assets/img/gallery/9.jpg" data-src="assets/img/gallery/9.jpg">
-			                
-			            </div>
+
 			            <div data-thumb="assets/img/gallery/10.jpg" data-src="assets/img/gallery/10.jpg">
-			                
+
 			            </div>
 			            <div data-thumb="assets/img/gallery/11.jpg" data-src="assets/img/gallery/11.jpg">
-			                
+
 			            </div>
 			        </div>
 
 			</div>
-	
+
 		</div>
 
 
@@ -127,12 +127,12 @@
 </section>
 
 
-		
+
 
 								<hr>
 								<br>
-								
-							
+
+
 
 
 <?php include('footer.php'); ?>
@@ -140,7 +140,7 @@
 
 <script>
 		jQuery(function(){
-			
+
 			// jQuery('#camera_wrap_1').camera({
 			// 	thumbnails: true
 			// });
